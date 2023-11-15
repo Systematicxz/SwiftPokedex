bf6bd0d12d070e9e0c8b6c5ed3ce0c14ce97ab0b
Ezequiel Barreto <eaviles@etsy.com>
2023-11-15 14:02:17 -0600

    feat: changes with more formatting

--- Pokedex/PokemonListViewController.swift
+++ Pokedex/PokemonListViewController.swift
@@ -7,28 +7,41 @@
 
 import UIKit
 
-class PokemonListViewController: UIViewController, PokemonManagerDelegate {
-    func showListPokemon(list: [Pokemon]) {
-        
-    }
-    
+class PokemonListViewController: UIViewController {
+
     var pokemonManager = PokemonManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+    
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var pokemonTable: UITableView!
+    @IBOutlet weak var searchBarPokemon: UISearchBar!
+
+    private func setupView() {
         pokemonManager.delegate = self
         pokemonTable.delegate = self
         pokemonTable.dataSource = self
         pokemonManager.showPokemon()
     }
-    
-    // IBOutlets
-
-    @IBOutlet weak var pokemonTable: UITableView!
-    @IBOutlet weak var searchBarPokemon: UISearchBar!
 }
 
-extension PokemonListViewController: UITableViewDelegate, UITableViewDataSource {
+// MARK: - PokemonManagerDelegate
+
+extension PokemonListViewController: PokemonManagerDelegate {
+
+    func showListPokemon(list: [Pokemon]) {
+        
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension PokemonListViewController: UITableViewDelegate {
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
     }
@@ -41,6 +54,10 @@ extension PokemonListViewController: UITableViewDelegate, UITableViewDataSource 
         cell.textLabel?.text = "Example"
         return cell
     }
-    
+}
+
+// MARK: - UITableViewDataSource
+
+extension PokemonListViewController: UITableViewDataSource {
     
 }
