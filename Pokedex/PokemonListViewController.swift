c8a6dd11c290f2727ff5287f3ecb9d09ea3ac0f3
Pedro Mendez <tostado_mt@hotmail.com>
2023-11-16 21:10:59 -0600

    feature PDX-3: search

--- Pokedex/PokemonListViewController.swift
+++ Pokedex/PokemonListViewController.swift
@@ -11,18 +11,26 @@ class PokemonListViewController: UIViewController, PokemonManagerDelegate {
     func showListPokemon(list: [Pokemon]) {
         self.pokemonList = list
         DispatchQueue.main.async {
+            self.pokemonFilter = self.pokemonList
             self.pokemonTable.reloadData()
         }
     }
+    
     var pokemonList: [Pokemon] = []
     var pokemonManager = PokemonManager()
+    
+    var pokemonFilter: [Pokemon] = []
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         pokemonManager.delegate = self
         pokemonTable.delegate = self
         pokemonTable.dataSource = self
+        searchBarPokemon.delegate = self
         pokemonManager.showPokemon()
+        pokemonFilter = pokemonList
     }
     
     // IBOutlets
@@ -33,7 +41,7 @@ class PokemonListViewController: UIViewController, PokemonManagerDelegate {
 
 extension PokemonListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pokemonList.count
+        return pokemonFilter.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -41,7 +49,24 @@ extension PokemonListViewController: UITableViewDelegate, UITableViewDataSource 
             withIdentifier: "cell",
             for: indexPath
         )
-        cell.textLabel?.text = pokemonList[indexPath.row].name
+        cell.textLabel?.text = pokemonFilter[indexPath.row].name
         return cell
     }
 }
+
+extension PokemonListViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        pokemonFilter = []
+        if searchText == "" {
+            pokemonFilter = pokemonList
+        } else {
+                for pokemon in pokemonList {
+                    if pokemon.name.lowercased().contains(searchText.lowercased()) {
+                        pokemonFilter.append(pokemon)
+                    }
+                }
+            }
+        self.pokemonTable.reloadData()
+        }
+    }
